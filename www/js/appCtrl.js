3c125311e8bd867f565acfdba4edbbec093ab84f
Alex Craven <w.alex.craven@gmail.com>
2016-10-28 18:13:38 -0500

    added multiple views and linked login/register to db

--- www/js/appCtrl.js
+++ www/js/appCtrl.js
@@ -8,7 +8,13 @@ angular.module('calendarApp', ['ionic', 'ngAnimate', 'ui.rCalendar'])
         $stateProvider
             .state('login', {
               url: '/',
-              templateUrl: 'templates/login.html'
+              templateUrl: 'templates/login.html',
+              controller: 'LoginCtrl'
+            })
+            .state('register', {
+              url: '/register',
+              templateUrl: 'templates/register.html',
+              controller: 'RegisterCtrl'
             })
             .state('tabs', {
                 url: '/tab',
@@ -24,24 +30,40 @@ angular.module('calendarApp', ['ionic', 'ngAnimate', 'ui.rCalendar'])
                     }
                 }
             })
-            .state('tabs.about', {
-                url: '/about',
+            .state('tabs.patients', {
+                url: '/patients',
                 views: {
-                    'about-tab': {
-                        templateUrl: 'templates/about.html'
+                    'patients-tab': {
+                        templateUrl: 'templates/patients.html'
                     }
                 }
             })
-            .state('tabs.contact', {
-                url: '/contact',
+            .state('tabs.addPatient', {
+                url: '/addPatient',
                 views: {
-                    'contact-tab': {
-                        templateUrl: 'templates/contact.html'
+                    'addPatient-tab': {
+                        templateUrl: 'templates/addPatient.html'
+                    }
+                }
+            })
+            .state('tabs.mileage', {
+                url: '/mileage',
+                views: {
+                    'mileage-tab': {
+                        templateUrl: 'templates/mileage.html'
+                    }
+                }
+            })
+            .state('tabs.settings', {
+                url: '/settings',
+                views: {
+                    'settings-tab': {
+                        templateUrl: 'templates/settings.html'
                     }
                 }
             });
 
-        $urlRouterProvider.otherwise('/');
+        // $urlRouterProvider.otherwise('/');            //uncomment after debugging
     })
 
     .controller('CalendarCtrl', function ($scope) {
@@ -80,4 +102,50 @@ angular.module('calendarApp', ['ionic', 'ngAnimate', 'ui.rCalendar'])
             console.log('Selected time: ' + selectedTime + ', hasEvents: ' + (events !== undefined && events.length !== 0));
         };
 
-    });
+    })
+
+    .controller('LoginCtrl', function($scope, $http, $location) {
+      $scope.data = {};
+
+      $scope.login = function() {
+        console.log("LOGIN user: " + $scope.data.Username + " - PW: " + $scope.data.Password);
+        $http.post('https://therassist.herokuapp.com/api/login', $scope.data)
+        .then(
+          (response) => {
+            if (response.data.status === 'OK') {
+              console.log('redirect to calendar');
+              $location.path('/tab/calendar')
+              // $scope.$apply()
+            } else {
+              $scope.userMessage = 'Username or Password does not match'
+            }
+            console.log(response.data);
+        },(error) => {
+          console.log(error);
+          $scope.userMessage = 'There was an error, please try again'
+        });
+      }
+    })
+
+    .controller('RegisterCtrl', function($scope, $http) {
+      $scope.data = {};
+
+      $scope.register = function() {
+        if ($scope.data.Username && $scope.data.Password && $scope.data.FullName) {
+          console.log("REGISTER user: " + $scope.data.Username + " - PW: " + $scope.data.Password + " - Name: " + $scope.data.FullName );
+          $http.post('https://therassist.herokuapp.com/api/REGISTER', $scope.data)
+          .then(
+            (response) => {
+              console.log(response.data);
+              $location.path('/tab/calendar')
+            },(error) => {
+              console.log(error);
+              $scope.userMessage = 'There was an error, please try again'
+            });
+        } else if ($scope.data.Password.length < 8) {
+          $scope.userMessage = 'Password must be at least 8 characters'
+        } else {
+          $scope.userMessage = 'Please fill out all fields'
+        }
+      }
+    })
